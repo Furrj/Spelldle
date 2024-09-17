90a0b5e8bfe1df54415393343944d2142b185e24
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-17 15:57:35 -0400

    added gameSessionID to UserData type

--- server/shared/types/userData.go
+++ server/shared/types/userData.go
@@ -3,11 +3,12 @@ package types
 type UserID uint
 
 type UserData struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Salt      string `json:"salt"`
-	Role      string `json:"role"`
-	UserID    UserID `json:"user_id"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
+	Username      string `json:"username"`
+	Password      string `json:"password"`
+	Salt          string `json:"salt"`
+	Role          string `json:"role"`
+	GameSessionID string `json:"game_session_id"`
+	UserID        UserID `json:"user_id"`
 }
